f0d29e57aa2e3eb2189fbb4d6e82fca3e816de10
Josh Robson Chase <josh@robsonchase.com>
2022-10-20 11:38:51 -0400

    Add development tool definitions

---
+++ flake.nix
@@ -0,0 +1,47 @@
+{
+  description = "ngrok agent library in Rust";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+
+    # Note: fenix packages are cached via cachix:
+    #       cachix use nix-community
+    fenix-flake = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, fenix-flake, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [
+            fenix-flake.overlay
+          ];
+        };
+      in
+      {
+        devShell = pkgs.mkShell {
+          CHALK_OVERFLOW_DEPTH = 3000;
+          CHALK_SOLVER_MAX_SIZE = 1500;
+          buildInputs = with pkgs; [
+            (fenix.complete.withComponents [
+              "cargo"
+              "clippy"
+              "rust-src"
+              "rustc"
+              "rustfmt"
+            ])
+            protobuf
+            go_1_18
+          ];
+        };
+      });
+}
